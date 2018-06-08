252eca84e21ba51a85efdcb5cf58c95b027c5eb5
Fred <fredpaw@gmail.com>
2018-06-08 16:52:34 +1000

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Andy',
-    text: 'This is Andy'
-  })
 });
 
 socket.on('disconnect', function() {
