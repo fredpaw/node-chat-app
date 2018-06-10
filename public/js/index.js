5b8454669795f915dba2d5b2561d8eb4b24a71b9
Fred Zhou <fredpaw@gmail.com>
2018-06-10 15:49:52 +1000

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
-
+    messageTextbox.val('');
   })
 })
 
@@ -42,14 +44,17 @@ locationButton.on('click', function(e) {
   if(!navigator.geolocation) {
     return alert('Geolocation is not supported!');
   }
-  e.preventDefault();
+  
+  locationButton.attr('disabled', 'disabled').text('Send location...');
 
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   })
 });
